e9d54d397dc6bec7aa9f41394359c7c951aae25b
aFku <zapasowyrowerek@gmail.com>
2022-08-17 23:05:53 +0200

    Add spring project and configure database

---
+++ src/test/java/com/rcbg/afku/investmentdiary/InvestmentDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcbg.afku.investmentdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
